ec994876eb540868ce94d791e9828acdb815879a
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-07-14 07:54:26 +0100

    chore: Mount '/logout' route and missing page middleware

--- server.js
+++ server.js
@@ -81,6 +81,15 @@ myDB(async client => {
     res.render('profile', { username: req.user.username });
   });
 
+  app.route('/logout'). get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
 }).catch(error => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
