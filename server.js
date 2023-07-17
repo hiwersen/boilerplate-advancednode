2be1c7f8ef44e6d71ae93584a1f68fcd64c1df32
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-07-17 17:35:59 +0100

    chore: Authentication with Socket.IO

--- server.js
+++ server.js
@@ -10,11 +10,40 @@ const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const routes = require('./routes.js');
 const auth = require('./auth.js');
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+
+const URI = process.env.MONGO_URI || 'mongodb://localhost:27017/chatFCC';
+const store = new MongoStore({ url: URI });
+
 
 const app = express();
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+  );
 
 
 app.set('view engine', 'pug');
@@ -22,9 +51,11 @@ app.set('views', './views/pug');
 
 // First: set up the express app to use the session by defining the following options
 app.use(session({ 
+  key: 'express.sid',
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
+  store: store,
   cookie: { 
     secure: false,
     httpOnly: true,
@@ -54,7 +85,7 @@ myDB(async client => {
     io.emit('user count', currentUsers);
 
     socket.on('disconnect', () => {
-      console.log('A user has disconnected');
+      console.log(`User ${socket.request.user.username} has disconnected`);
       --currentUsers;
       io.emit('user count', currentUsers);
     });
